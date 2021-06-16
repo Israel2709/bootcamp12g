6b119ae789281f9c7c48054f0535e552872917d5
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-06-15 21:37:18 -0500

    javascript s1

---
+++ s9javascript/js/main.js
@@ -0,0 +1,81 @@
+//alert("hola koders!")
+console.log("hola koders desde consola")
+console.error("Houston, tenemos un problema")
+console.warn("cuidado!")
+console.log()
+
+//prompt("Ingresa tu nombre:")
+
+/*
+    operadores de asignación:
+    = -> Asignación
+    += -> Asignación con adición
+    -= -> Asignación con sustracción
+    *= -> Asignación con multiplicación
+    /= -> Asignación con división
+    %= -> Asignación con módulo
+*/
+
+var foo /*declaración*/
+
+foo = "Lestat" /* asignación */
+
+/*
+var name = prompt("ingresa tu nombre")
+var lastName = prompt("Ingresa tu apellido")
+
+console.log( name )
+console.log( lastName )
+
+console.log('nombre completo: ' + name +  lastName )
+
+var age = 31
+
+console.log( typeof name )
+console.log( typeof age )
+*/
+var someNumber = 5
+var someOtherNumber = 7
+
+console.log( someNumber + someOtherNumber )
+console.log( someNumber - someOtherNumber )
+console.log( someNumber * someOtherNumber )
+//console.log( someNumber / someOtherNumber )
+//console.log( someNumber % someOtherNumber )
+
+var thirdNumber = "37"
+
+//console.log( someNumber + thirdNumber )
+//console.log( thirdNumber + someNumber )
+
+/*operadores de comparación:
+
+== comparación
+=== extrema comparación
+!= diferente
+!== extremadamente diferente
+> mayor que 
+< menor que
+>= mayor o igual que
+<= menor o igual que
+*/
+
+var testNumber = 12
+var testStringNumber = "12"
+
+//console.log( testNumber == testStringNumber)
+/*true*/
+
+//console.log( testNumber === testStringNumber)
+/*false*/
+
+//console.log( testNumber != testStringNumber )
+/*false*/
+
+//console.log( testNumber !== testStringNumber )
+/*true*/
+
+var string1 = "a"
+var string2 = "b"
+
+console.log(   string1 > string2 )

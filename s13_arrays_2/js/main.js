4fa60a93fbc40ebce841d793eba1df5e71f82885
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-06-23 20:26:34 -0500

    songs practice

--- s13_arrays_2/js/main.js
+++ s13_arrays_2/js/main.js
@@ -1,163 +1,233 @@
-/*function sumar( a, b ){
-  return a + b
-}*/
-
-//let result1 = sumar(4,5)
-
-/*let result1 = function(a,b){
-  return a + b 
-}
-
-console.log( result1( 4, 9) )*/
-
-/*const sumar = ( a, b ) => {
-  return a + b
-}*/
-
-const sumar = ( a, b ) => a + b
-
-//console.log( sumar( 4, 8) )
-
-let result = sumar( 7 , 2 )
-
-console.log( result )
-
-let someArray = [
-  "Israel Salinas",
-  "David Moranchel",
-  "Carlos Silva",
-  "Michael Villalba"
+/*usando mentorsArray, realizar lo siguiente:
+-Obtener el score promedio de cada materia( HTML, CSS, JS, ReactJS )
+-Obtener el promedio individual de cada mentor
+-crear un array de strings con la siguiente forma:
+     "Mi nombre es {nombre} y mi promedio es de {promedio}"
+-Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+*/
+var mentorsArray = [
+  {
+    name: "Israel Salinas Martinez",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8
+      },
+      {
+        signature: "CSS",
+        score: 10
+      },
+      {
+        signature: "JS",
+        score: 8
+      },
+      {
+        signature: "ReactJS",
+        score: 8
+      }
+    ]
+  },
+  {
+    name: "David Cermeño Moranchel",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8
+      },
+      {
+        signature: "CSS",
+        score: 7
+      },
+      {
+        signature: "JS",
+        score: 10
+      },
+      {
+        signature: "ReactJS",
+        score: 10
+      }
+    ]
+  },
+  {
+    name: "Charles Silva",
+    scores: [
+      {
+        signature: "HTML",
+        score: 9
+      },
+      {
+        signature: "CSS",
+        score: 9
+      },
+      {
+        signature: "JS",
+        score: 10
+      },
+      {
+        signature: "ReactJS",
+        score: 9
+      }
+    ]
+  },
+  {
+    name: "Michael Villalba Sotelo",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8
+      },
+      {
+        signature: "CSS",
+        score: 10
+      },
+      {
+        signature: "JS",
+        score: 9
+      },
+      {
+        signature: "ReactJS",
+        score: 10
+      }
+    ]
+  }
 ]
 
-//splitNames tomará un array de strings
-const splitNames = ( namesArray ) => {
-  //Creará un nuevo array*/
-  let splittedNames = []
-  //y recorrerá cada uno de los items del array original ( namesArray )
-  for( let i = 0; i < namesArray.length; i++ ){
-    //por cada items
-    //dividirá el string en palabras ( split(" ") ) y agregará el resultado al nuevo array ( splittedNames)
-    splittedNames.push( namesArray[i].split(" ") )
-  }
+const getMentorsScores = mentors => {
+  let mentorsQty = mentors.length
+  let htmlTotal = 0;
+  let cssTotal = 0;
+  let jsTotal = 0;
+  let reactTotal = 0;
+  mentors.forEach(mentor => {
+    // console.log( mentor.scores )
+    let scores = mentor.scores
+    scores.forEach(score => {
+      let signature = score.signature
+      // console.log( 'signature', signature )
+      let value = score.score
+      // console.log( 'value', value)
+      switch (signature) {
+        case 'HTML':
+          htmlTotal += value
+          break
 
-  //al final devolveremos el nuevo array con los nombres divididos( splittendNames )
-  return splittedNames 
+        case 'CSS':
+          cssTotal += value
+          break
+
+        case 'JS':
+          jsTotal += value
+          break
+
+        case 'ReactJS':
+          reactTotal += value
+          break
+
+        default:
+        // console.log("Asignatura no registrada")
+      }
+    })
+  })
+  let htmlAverage = htmlTotal / mentorsQty
+  let cssAverage = cssTotal / mentorsQty
+  let jsAverage = jsTotal / mentorsQty
+  let reactAverage = reactTotal / mentorsQty
+  // console.log( htmlAverage )
+  // console.log( cssAverage )
+  // console.log( jsAverage )
+  // console.log( reactAverage )
+
+  return { htmlAverage, cssAverage, jsAverage, reactAverage }
 }
 
-//let otherArray = splitNames( someArray )
-
-//console.log( otherArray )
-
-//print items recibe un array
-const printItems = ( array ) => {
-  //e imprime cada uno de los items de ese array 
-  for( let i = 0; i < array.length; i++ ){
-    console.log( array[i])
-  }
+const getMentorAverage = scoresArray => {
+  let scoreTotal = 0;
+  let signaturesQty = scoresArray.length
+  scoresArray.forEach(mentorScore => {
+    let value = mentorScore.score
+    scoreTotal += value
+  })
+  let mentorAverage = scoreTotal / signaturesQty
+  // console.log('promedio del mentor:', mentorAverage )
+  return mentorAverage
 }
 
-/*const sumar = ( a, b ) => {
-  return a + b 
-}*/
+const createLabelsArray = mentorsData => {
+  let labelsArray = []
+  mentorsData.forEach(mentor => {
+    let mentorName = mentor.name
+    let mentorAverage = getMentorAverage(mentor.scores)
+    console.log('name ', mentorName, 'average ', mentorAverage)
+    let label = `Hola, soy ${mentorName} y mi promedio es de ${mentorAverage}`
+    labelsArray.push(label)
+  })
+  console.log(labelsArray)
+  return labelsArray
+}
+
+const getHighestAverages = mentorsData => {
+  let highestsAverages = []
+  mentorsData.forEach(mentor => {
+    let average = getMentorAverage(mentor.scores)
+    console.log(average)
+
+    average > 9 && highestsAverages.push(mentor)
+  })
+  console.log(highestsAverages)
+  return highestsAverages
+}
+
+getHighestAverages(mentorsArray)
+
+createLabelsArray(getHighestAverages(mentorsArray))
+
+//createLabelsArray( mentorsArray )
+
+//getMentorsScores( mentorsArray )
 
 
 
 
-printItems( someArray )
-
-//ejecutamos printItems usando como argumento el array que retorna splitNames
-printItems( splitNames( someArray ) /*[[],[],[]]*/) 
-
-
-let namesArray = [
-    "Israel Salinas",
-    "David Moranchel",
-    "Carlos Silva",
-    "Michael Villalba"
-  ]
-  /*regrese un número aleatorio desde 0 hasta array.length*/
-  
-  /*const getRandomIndex = array => {
-    return Math.floor( Math.random( ) * array.length )
-  }*/
-  
-  const getRandomIndex = array => Math.floor( Math.random( ) * array.length )
-  
-  let randomIndex = getRandomIndex( namesArray )
-  //console.log( randomIndex )
-  
-  /*const printNameByIndex = index => {
-    return namesArray[index]
-  }*/
-  
-  const printNameByIndex = ( indexToFind, arrayToSearch )  => arrayToSearch[indexToFind]
-  
-  let someName = printNameByIndex( getRandomIndex( namesArray ) , namesArray )
-  console.log( someName )
-  
-  
-  
-  const sumar = ( a, b ) => a + b
-  
-  let myObject = {
-    name:"Ikki",
-    alias:"El pájaro Dev"
+let songsData = [
+  {
+    name: "Kashmir",
+    band: "Led Zeppelin",
+    releaseYear: "1980",
+    statistics: {
+      likes: 20000,
+      reproductions: 8000,
+    }
+  },
+  {
+    name: "Smells Like Teen Spirit",
+    band: "Nirvana",
+    releaseYear: "1989",
+    statistics: {
+      likes: 25000,
+      reproductions: 100000,
+    }
+  }, {
+    name: "So What",
+    band: "Metallica",
+    releaseYear: "1990",
+    statistics: {
+      likes: 12000,
+      reproductions: 95000,
+    }
   }
-  
-  let birdName = myObject.name
-  let birdAlias = myObject.alias
-  
-  /*destructuring*/
-  
-  let { birdName:name, birdAlias:alias } = myObject
-  
-  console.log( birdName, birdAlias)
-  
-  
-  
-  
-  //const results = {}
-  //console.log( results )
-  
-  //results.result1 = sumar(2 , 2)
-  //results.result2 = sumar(2 , 6)
-  
-  //console.log( results )
-  
-  const results = { result1:sumar(2,2), result2: sumar(2,6) }
-  console.log( results )
-  
-  let firstname = "israel"
-  let lastName = "salinas martínez"
-  let age = 31
-  
-  let person = { name:firstname, lastName, age }
-  
-  console.log( person )
+]
 
+/*agrupar únicamente los nombres de las bandas, sin repeticiones*/
 
-  let dataArray = [
-      [
-          "Israel",
-          "Salinas Martínez"
-      ],
-      [
-          "David",
-          "Cermeño Moranchel"
-      ],
-      [
-          "Charles",
-          "Silva"
-      ],
-      [
-          "Michael",
-          "Villalba Sotelo"
-      ]
-  ]
+/*
+  Agrupar las canciones de cada banda
+*/
 
-  /* mentorsArray = [
-      {
-          name:"Israel",
-          lastName:"salinas"
-      }...{...}
-  ]*/
+/*
+  saber cuál es la canción con más likes
+  ( nombre de la canción y nombre de la banda)
+  */
+
+/*saber cuál es la canción con más reproducciones
+( nombre de la canción y nombre de la banda)
+*/

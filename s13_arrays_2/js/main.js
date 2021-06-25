8b9c4ba0b55f2129c778b5eb9159651361f290a0
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-06-25 16:38:14 -0500

    .

--- s13_arrays_2/js/main.js
+++ s13_arrays_2/js/main.js
@@ -1,193 +1,3 @@
-/*usando mentorsArray, realizar lo siguiente:
--Obtener el score promedio de cada materia( HTML, CSS, JS, ReactJS )
--Obtener el promedio individual de cada mentor
--crear un array de strings con la siguiente forma:
-     "Mi nombre es {nombre} y mi promedio es de {promedio}"
--Obtener la lista de mentores cuyo promedio sea mayor a 9.5
-*/
-var mentorsArray = [
-  {
-    name: "Israel Salinas Martinez",
-    scores: [
-      {
-        signature: "HTML",
-        score: 8
-      },
-      {
-        signature: "CSS",
-        score: 10
-      },
-      {
-        signature: "JS",
-        score: 8
-      },
-      {
-        signature: "ReactJS",
-        score: 8
-      }
-    ]
-  },
-  {
-    name: "David Cermeño Moranchel",
-    scores: [
-      {
-        signature: "HTML",
-        score: 8
-      },
-      {
-        signature: "CSS",
-        score: 7
-      },
-      {
-        signature: "JS",
-        score: 10
-      },
-      {
-        signature: "ReactJS",
-        score: 10
-      }
-    ]
-  },
-  {
-    name: "Charles Silva",
-    scores: [
-      {
-        signature: "HTML",
-        score: 9
-      },
-      {
-        signature: "CSS",
-        score: 9
-      },
-      {
-        signature: "JS",
-        score: 10
-      },
-      {
-        signature: "ReactJS",
-        score: 9
-      }
-    ]
-  },
-  {
-    name: "Michael Villalba Sotelo",
-    scores: [
-      {
-        signature: "HTML",
-        score: 8
-      },
-      {
-        signature: "CSS",
-        score: 10
-      },
-      {
-        signature: "JS",
-        score: 9
-      },
-      {
-        signature: "ReactJS",
-        score: 10
-      }
-    ]
-  }
-]
-
-const getMentorsScores = mentors => {
-  let mentorsQty = mentors.length
-  let htmlTotal = 0;
-  let cssTotal = 0;
-  let jsTotal = 0;
-  let reactTotal = 0;
-  mentors.forEach(mentor => {
-    // console.log( mentor.scores )
-    let scores = mentor.scores
-    scores.forEach(score => {
-      let signature = score.signature
-      // console.log( 'signature', signature )
-      let value = score.score
-      // console.log( 'value', value)
-      switch (signature) {
-        case 'HTML':
-          htmlTotal += value
-          break
-
-        case 'CSS':
-          cssTotal += value
-          break
-
-        case 'JS':
-          jsTotal += value
-          break
-
-        case 'ReactJS':
-          reactTotal += value
-          break
-
-        default:
-        // console.log("Asignatura no registrada")
-      }
-    })
-  })
-  let htmlAverage = htmlTotal / mentorsQty
-  let cssAverage = cssTotal / mentorsQty
-  let jsAverage = jsTotal / mentorsQty
-  let reactAverage = reactTotal / mentorsQty
-  // console.log( htmlAverage )
-  // console.log( cssAverage )
-  // console.log( jsAverage )
-  // console.log( reactAverage )
-
-  return { htmlAverage, cssAverage, jsAverage, reactAverage }
-}
-
-const getMentorAverage = scoresArray => {
-  let scoreTotal = 0;
-  let signaturesQty = scoresArray.length
-  scoresArray.forEach(mentorScore => {
-    let value = mentorScore.score
-    scoreTotal += value
-  })
-  let mentorAverage = scoreTotal / signaturesQty
-  // console.log('promedio del mentor:', mentorAverage )
-  return mentorAverage
-}
-
-const createLabelsArray = mentorsData => {
-  let labelsArray = []
-  mentorsData.forEach(mentor => {
-    let mentorName = mentor.name
-    let mentorAverage = getMentorAverage(mentor.scores)
-    console.log('name ', mentorName, 'average ', mentorAverage)
-    let label = `Hola, soy ${mentorName} y mi promedio es de ${mentorAverage}`
-    labelsArray.push(label)
-  })
-  console.log(labelsArray)
-  return labelsArray
-}
-
-const getHighestAverages = mentorsData => {
-  let highestsAverages = []
-  mentorsData.forEach(mentor => {
-    let average = getMentorAverage(mentor.scores)
-    console.log(average)
-
-    average > 9 && highestsAverages.push(mentor)
-  })
-  console.log(highestsAverages)
-  return highestsAverages
-}
-
-getHighestAverages(mentorsArray)
-
-createLabelsArray(getHighestAverages(mentorsArray))
-
-//createLabelsArray( mentorsArray )
-
-//getMentorsScores( mentorsArray )
-
-
-
-
 let songsData = [
   {
     name: "Kashmir",
@@ -241,17 +51,102 @@ let songsData = [
   }
 ]
 
-/*agrupar únicamente los nombres de las bandas, sin repeticiones*/
+const getBands = ( arrayAiterar ) => {
+  let bandsList = []
+  arrayAiterar.forEach( ( item ) => {
+    let bandName = item.band.toLowerCase()
+    !bandsList.includes( bandName ) && bandsList.push( bandName )
+
+    if( !bandsList.includes( bandName ) ){
+      bandsList.push( bandName )
+    }
+
+    
+
+  })
+  return bandsList
+}
+
+//getBands(songsData )
+
+const groupBands = ( arrayAiterar ) => {
+  let bandsList = getBands( arrayAiterar )
+  console.log( bandsList)
+  let songsGroup = {}
+
+
+  bandsList.forEach( band => {
+    console.log(band)
+    console.log( band.toLowerCase().replaceAll(" ","_") )
+    songsGroup[band.toLowerCase().replaceAll(" ","_")] = []
+  })
+ 
+
+  /*arrayAiterar.forEach( song => {
+    switch( song.band.toLowerCase() ){
+      case "metallica":
+        songsGroup.metallica.push(song)
+        break
+    }
+  })*/
+
+  console.log( songsGroup )
+}
+
+groupBands( songsData )
 
 /*
-  Agrupar las canciones de cada banda
+  [
+    "La canción { cancion } es de { banda} "
+  ]
 */
+
+const createLabelsArray = arrayAiterar => {
+  let labelsArray = []
+  arrayAiterar.forEach( song => {
+    let { name, band } = song
+    let label = `La canción ${ name} es de ${ band }`
+    labelsArray.push( label )
+    console.log( label )
+  })
+  return labelsArray
+}
+
+let songLabels = songsData.map( ({band, name} ) => `La canción ${ name } es de ${ band }`)
+
+let result = createLabelsArray( songsData )
+
+console.log( result )
 
 /*
-  saber cuál es la canción con más likes
-  ( nombre de la canción y nombre de la banda)
-  */
-
-/*saber cuál es la canción con más reproducciones
-( nombre de la canción y nombre de la banda)
+let songLabels = songsData.map( song => {
+  let { name, band } = song 
+  let label = `La canción ${ name} es de ${ band }`
+  return label
+})
 */
+                          
+console.log( songLabels )
+
+const getSongByBand = songsData => {
+  console.log( songsData )
+  let result = []
+  songsData.forEach( song => {
+    let { band } = song
+    console.log( band )
+    band === "Metallica" && result.push( song )
+  })
+  console.log( result )
+  return result
+}
+
+/*
+let songsByBand = songsData.filter( song => {
+    return song.band === "Nirvana"
+})*/
+
+let songsByBand = songsData.filter( ({band}) => band === 'Nirvana')
+
+console.log( songsByBand )
+
+//getSongByBand( songsData )

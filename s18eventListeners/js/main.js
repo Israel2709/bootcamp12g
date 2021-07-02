c2e2c38b19d45003d7764eaf1c95a4fffbc3b7e9
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-07-01 21:33:53 -0500

    person table2

--- s18eventListeners/js/main.js
+++ s18eventListeners/js/main.js
@@ -42,5 +42,54 @@ let headerInput = document.getElementById("header-input")
 headerInput.addEventListener("keyup", (event) => {
     let textValue = event.target.value
     console.log( textValue )
-    document.querySelector("form h1").textContent = textValue
+    let header = document.querySelector("form h1")
+    header.textContent = textValue
 })
+
+
+let personArray = [
+    {
+        name:"Ikki",
+        lastname:"El pájaro dev"
+    },
+    {
+        name:"Hagen Isaac",
+        lastname:"Salinas Corona"
+    }
+]
+
+personArray.forEach( (person, index) =>  {
+    let {name, lastname } = person 
+    let personRow = document.createElement("tr")
+    //<tr></tr>
+    let indexTd = document.createElement("td")
+    //<td></td>
+    let nameTd = document.createElement("td")
+    //<td></td>
+    let lastnameTd = document.createElement("td")
+    //<td></td>
+    let buttonTd = document.createElement("td")
+
+    let nameText = document.createTextNode( name )
+    let lastnameText = document.createTextNode( lastname )
+    let indexText = document.createTextNode( index + 1 )
+    let deleteButton = document.createElement("button")
+    deleteButton.classList.add("btn", "btn-warning")
+
+    let buttonText = document.createTextNode("Borrar")
+    deleteButton.appendChild(buttonText)
+
+    buttonTd.appendChild(deleteButton)
+
+    indexTd.appendChild(indexText)
+    nameTd.appendChild(nameText)
+    lastnameTd.appendChild(lastnameText)
+
+    personRow.appendChild( indexTd )
+    personRow.appendChild( nameTd )
+    personRow.appendChild( lastnameTd )
+    personRow.appendChild( buttonTd )
+
+    document.getElementById("person-table").appendChild( personRow )
+})
+

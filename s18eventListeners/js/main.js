29656b358b16e12c7006d344bdd47b86453cc651
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-07-02 21:06:12 -0500

    dom creation

--- s18eventListeners/js/main.js
+++ s18eventListeners/js/main.js
@@ -1,52 +1,4 @@
-let count = 0;
-
-/*let botonArriba = document.getElementById("increment")
-
-botonArriba.addEventListener("click", () => {
-    console.log("click")
-    count++
-
-    document.getElementById("counter").textContent = count
-    console.log( count )
-})
-
-let botonAbajo = document.getElementById("decrement")
-
-botonAbajo.addEventListener("click", () => {
-    count--
-    document.getElementById("counter").textContent = count
-})*/
-
-let buttons = document.querySelectorAll(".btn")
-console.log( buttons )
-
-buttons.forEach( button => {
-    //console.log( button )
-    button.addEventListener("click", ( event ) => {
-        console.log( event )
-        console.log( event.target )
-        console.log( event.target.id )
-        //console.log("button")
-        let elementId = event.target.id
-
-        elementId === "increment" ? count++ : count--
-        document.getElementById("counter").textContent = count
-     } )
-})
-
-
-
-
-let headerInput = document.getElementById("header-input")
-
-headerInput.addEventListener("keyup", (event) => {
-    let textValue = event.target.value
-    console.log( textValue )
-    let header = document.querySelector("form h1")
-    header.textContent = textValue
-})
-
-
+/*
 let personArray = [
     {
         name:"Ikki",
@@ -92,4 +44,71 @@ personArray.forEach( (person, index) =>  {
 
     document.getElementById("person-table").appendChild( personRow )
 })
+*/
 
+let personArray = []
+
+let saveBtn = document.getElementById("save-person")
+
+saveBtn.addEventListener("click", () => {
+    let personObject = {}
+    let fields = document.querySelectorAll("#person-form input")
+    //console.log( fields )
+    fields.forEach(field => {
+        let property = field.name
+        let value = field.value
+        console.log(property, value)
+        personObject = { ...personObject, [property]: value }
+    })
+    console.log(personObject)
+    personArray.push(personObject)
+    console.log(personArray)
+    printTable( personArray )
+})
+
+const deletePerson = event => {
+    let personIndex = event.target.dataset.personIndex
+    personArray.splice(personIndex,1)
+    printTable( personArray )
+}
+
+const printTable = dataToPrint => {
+    document.getElementById("person-table").innerHTML = ""
+    dataToPrint.forEach((item,index) => {
+        let { nombre, apellidos } = item
+
+        let personRow = document.createElement("tr")
+        //<tr></tr>
+        let indexTd = document.createElement("td")
+        //<td></td>
+        let nameTd = document.createElement("td")
+        //<td></td>
+        let lastnameTd = document.createElement("td")
+        //<td></td>
+        let buttonTd = document.createElement("td")
+
+        let nameText = document.createTextNode(nombre)
+        let lastnameText = document.createTextNode(apellidos)
+        let indexText = document.createTextNode(index + 1)
+        let deleteButton = document.createElement("button")
+        deleteButton.classList.add("btn", "btn-warning")
+        deleteButton.dataset.personIndex = index
+        deleteButton.addEventListener("click", deletePerson )
+
+        let buttonText = document.createTextNode("Borrar")
+        deleteButton.appendChild(buttonText)
+
+        buttonTd.appendChild(deleteButton)
+
+        indexTd.appendChild(indexText)
+        nameTd.appendChild(nameText)
+        lastnameTd.appendChild(lastnameText)
+
+        personRow.appendChild(indexTd)
+        personRow.appendChild(nameTd)
+        personRow.appendChild(lastnameTd)
+        personRow.appendChild(buttonTd)
+
+        document.getElementById("person-table").appendChild(personRow)
+    })
+}

3c850527b65fa86ab9f331e5b3c2b29581493069
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-07-12 20:47:34 -0500

    jquery ajax

--- s20ajaxJquery/js/main.js
+++ s20ajaxJquery/js/main.js
@@ -5,11 +5,29 @@ const printDivisibles = () => {
     for( let i = 0; i < limit; i++){
         i % divisor === 0 && printCard( i )
     }
+   
     $(".number-card").click( ( event ) => {
+        let result = 1
         event.stopPropagation()
         console.log( event.target)
-        $(event.target).addClass("active")
+        $(event.target).toggleClass("active")
+        $(".active").each( function(index){
+            console.log( index )
+            console.log( $(this).find("h1").text() )
+            let value = parseInt($(this).find("h1").text())
+            console.log( value )
+            result *= value 
+        })
+        console.log( result )
+
+        let activeCards = document.querySelectorAll(".active h1")
+        activeCards.forEach( card => {
+            let value = parseInt(card.text)
+            result *= value
+        })
+
     })
+   
 }
 
 const printCard = number => {
@@ -26,3 +44,53 @@ const printCard = number => {
 }
 
 $("#go").click( printDivisibles )
+
+
+$(".element").toggleClass("d-none")
+
+document.querySelectorAll(".element").forEach( element => {
+    element.classList.includes("d-none") ? element.classList.remove("d-none") : element.classList.add("d-none")
+})
+
+let someObject = {
+    "name":"israel Salians Martinez",
+    "phone":"5543788096"
+}
+
+console.log( someObject )
+console.log( `some Object`, someObject )
+console.log( someObject )
+
+
+
+objeto = {
+    "key":{
+        "name":"asdfasd",
+        "lastname":"asdfadvasd"
+    }
+}
+
+const bringPosts = (endpoint)=> {
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        let myArr = []
+        if (this.readyState == 4 && this.status == 200) {
+            let objeto = JSON.parse(this.responseText);
+            for(let property in objeto){
+                // Imprimimos el objeto pero este regresa en string como object
+                console.log(`${property}: ${objeto[property]}`);
+                //Aqui al entrar a un arreglo, el objeto regresa completo
+                myArr.push(objeto[property]);
+            }
+            //console.log(JSON.parse(this.responseText));
+            console.log(myArr);
+        }
+    };
+
+    xhttp.open("GET", endpoint, true);
+    xhttp.send();
+}
+
+
+bringPosts(endpoint)
---
+++ s21jquery_ajax/js/main.js
@@ -0,0 +1,89 @@
+//https://blog-12g-default-rtdb.firebaseio.com/.json
+
+const BASE_URL = 'https://blog-12g-default-rtdb.firebaseio.com/israel'
+const savePet = petData => {
+    $.ajax({
+        method:"POST",
+        url:`${BASE_URL}/pets.json`,
+        data:JSON.stringify( petData ),
+        success: response => {
+            console.log( response )
+        },
+        error: error => {
+            console.log( "hay un error ")
+            console.log( error )
+        },
+        async:false
+    })
+}
+
+const getAllPets = () => {
+    let result
+    $.ajax({
+        method:"GET",
+        url:`${BASE_URL}/pets.json`,
+        success: response => {
+            result = response
+        },
+        error: error => {
+            console.log( "hay un error ")
+            console.log( error )
+        },
+        async:false
+    })
+    return result
+}
+
+const getPetById = petId => {
+    let result
+    $.ajax({
+        method:"GET",
+        url:`${BASE_URL}/pets/${petId}.json`,
+        success: response => {
+            result = response
+        },
+        error: error => {
+            console.log( "hay un error ")
+            console.log( error )
+        },
+        async:false
+    })
+    return result
+}
+
+$("#save-pet").click(() => {
+    let petObject = {}
+    $("#pet-form input").each( function(){
+        let property = $(this).attr("name")
+        let value = $(this).val()
+        console.log(property, value )
+        petObject = {...petObject, [property] : value }
+    })
+    console.log( petObject )
+    savePet( petObject )
+    printAllPets()
+})
+
+const printAllPets = () => {
+    $(".pets-wrapper").empty()
+    let allPets = getAllPets()
+    for( pet in allPets ){
+        let { name, specie, age, picture } = allPets[pet]
+
+        let petHtml = `
+        <div class="col-12 col-md-6">
+        <div class="card" style="width: 18rem;">
+            <img src=${picture} class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">Especie: ${specie}</p>
+                <p class="card-text">Edad: ${age}</p>
+                <a href="#" class="btn btn-primary" data-pet-key=${pet}>Go somewhere</a>
+            </div>
+            </div>
+            </div>
+        `
+        $(".pets-wrapper").append(petHtml)
+    }
+}
+
